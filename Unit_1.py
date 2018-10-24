2ab5fa5d0be0ae8380908bf9f131477fbb930a04
Pavan Murugesh <pavan.murugesh@student.rjuhsd.us>
2018-10-24 20:33:19 +0000

    Unit-!

---
+++ Unit_1.py
@@ -0,0 +1,55 @@
+print(36/7)
+
+print('Hi', input())# hi john
+
+a=int(input()) # squares
+print(a*a)
+
+a = int(input()) #area or a right triangle
+b= int(input())
+
+print(a*b/2)
+
+
+name = input()# hello harry
+
+# Print the result using:
+# print('Greeting', name)
+Greeting = 'Hello,'
+
+print(Greeting , name + '!')
+
+n = int(input()) # apple sharing
+k = int(input())
+
+# Print the result with print()
+
+# Example of division, integer division and remainder:
+print(k // n)
+
+print(k % n)
+
+
+a = int(input())# previous and next
+
+print("The next number for the number",a, "is",a+1,)
+print("The previous number for the number",a, "is",a-1,)
+
+
+a = float(input()) #two timestamps
+b = float(input())
+c = float(input())
+a1 = float(input())
+b1 = float(input())
+c1 = float(input())
+
+
+print(-((a*3600 + b*60 + c) - (a1*3600 + b1*60 + c1)) )
+
+a = int(input())#school desks
+b = int(input())
+c = int(input())
+
+print(a//2 + b//2 + c//2 + b%2 + c%2 + a%2 )
+
+
