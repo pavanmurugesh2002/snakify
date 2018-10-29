0e4e32e64f883fbb800b268933ebf821b8a87661
pavanmurugesh2002 <44304621+pavanmurugesh2002@users.noreply.github.com>
2018-10-29 13:41:06 -0700

    Update Unit_1.py

--- Unit_1.py
+++ Unit_1.py
@@ -53,3 +53,86 @@ c = int(input())
 print(a//2 + b//2 + c//2 + b%2 + c%2 + a%2 )
 
 
+#Unit 2
+a = int(input()) # last digit of an integer
+
+b = a%10
+
+print(b)
+
+
+a = int(input()) #tens digit
+
+b = ((a//10)%10)
+
+print(b)
+
+
+b = (a%10) # sum of digits
+c = ((a//10)%10)
+d = ((a//100)%10)
+
+print(b + c + d)
+
+
+a = float(input())# fractional parts
+print(a%1)
+
+
+b = float(input()) # first digit after decimal point
+print(int((b/0.1)%10))
+
+
+import math #Car Route
+
+from math import ceil
+
+N = int(input())
+
+M = int(input())
+
+
+N = int(input()) # Digital Clock
+
+a = (N//60)
+
+b = (N%60)
+
+print( a , b)
+
+
+A = int(input()) #total cost
+B = int(input())
+
+N = int(input())
+
+x = A*100
+t = N*(x + B)
+print(t//100, t%100)
+
+
+
+import math #clockface 1
+
+H = int(input())
+
+M = int(input())
+
+S = int(input())
+
+x = (H*3600) + (M*60) + S
+print(x*.008333333)
+
+
+a = float(input()) # clockface 2 
+
+x = a%30
+
+print(x*12)
+
+
+
+
+
+
+
